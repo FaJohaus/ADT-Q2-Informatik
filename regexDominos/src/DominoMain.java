5a944aa5e0fac4a6aececf2ece8c4c5dc50c7302
Fabian5150 <fabian.strauch.04@web.de>
2021-11-30 21:18:41 +0100

    bitchy program stopped crying and is now able to copy an array to an arraylist

--- regexDominos/src/DominoMain.java
+++ regexDominos/src/DominoMain.java
@@ -4,10 +4,26 @@ import java.util.List;
 
 public class DominoMain {
     public static void main(String[] args) {
-        for (String[] Domino : Dominos.getRegexDominos()) {
-            System.out.println(Domino[1]);
-        }
+        DominoKette();
+    }
+
+    public static ArrayList<String> DominoKette(){
+        ArrayList<String> DominoKette = new ArrayList<>();
 
         //Kopiere die Liste in eine ArrayList, die für diesen Versuch verhackstückelt werden kann
+        String [][] DominosArr = Dominos.getRegexDominos();
+        ArrayList<String[]> Dominos = new ArrayList<>(Arrays.asList(DominosArr));
+
+        /* for (String[] Domino : Dominos) {
+            System.out.println(Domino[1]);
+        } */
+
+        return DominoKette;
+    }
+
+    private boolean isMatch(String regex, String string){
+
+
+        return true;
     }
 }
