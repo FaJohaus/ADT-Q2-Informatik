619cf906164f3a7ade38a2bba94c3080454c1952
Fabian5150 <65031308+Fabian5150@users.noreply.github.com>
2022-01-24 11:51:52 +0100

    Delete klausurÜbungServer directory

--- klausurÜbungServer/src/gamblingServer/ServerMain.java
+++
@@ -1,9 +0,0 @@
-package gamblingServer;
-
-public class ServerMain {
-    public static void main(String[] args) {
-        int port = 8818;
-        GamblingServer server = new GamblingServer(port);
-        server.start();
-    }
-}
